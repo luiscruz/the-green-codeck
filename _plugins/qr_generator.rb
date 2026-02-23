c74d82ec52702495705072187bf8a0b3ac486df8
Luis Cruz <luismirandacruz@gmail.com>
2026-02-23 21:30:05 +0100

    add GitHub Actions deployment and auto-generate QR codes via plugin

---
+++ _plugins/qr_generator.rb
@@ -0,0 +1,24 @@
+require 'rqrcode'
+require 'fileutils'
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  base_url   = "#{site.config['url']}#{site.config['baseurl']}"
+  output_dir = File.join(site.dest, 'assets', 'qr')
+  FileUtils.mkdir_p(output_dir)
+
+  categories  = site.data['categories']
+  bgcolor_for = categories.each_with_object({}) { |c, h| h[c['name']] = c['background'] }
+
+  site.collections['the-patterns'].docs.each do |doc|
+    next unless doc.data['category']
+
+    slug    = File.basename(doc.path, '.md')
+    bgcolor = bgcolor_for[doc.data['category']] || 'ffffff'
+    url     = "#{base_url}/the-patterns/#{slug}/"
+
+    qr  = RQRCode::QRCode.new(url)
+    svg = qr.as_svg(color: '000000', fill: bgcolor, module_size: 4, standalone: true)
+
+    File.write(File.join(output_dir, "#{slug}.svg"), svg)
+  end
+end
